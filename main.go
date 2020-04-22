e1b297ef34c3d4bd3481aa6b0355d6f8f5598052
justin <justinkim0310@gmail.com>
2020-04-22 14:07:46 +1000

    first commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contact struct {
+	email    string
+	postcode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contact{
+			email:    "jim@gmail.com",
+			postcode: 94000,
+		},
+	}
+
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(firstName string) {
+	(*pointerToPerson).firstName = firstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
